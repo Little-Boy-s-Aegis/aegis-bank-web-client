577eea3abb0c405a97288a32972cc5bceae98a3c
L1nkinPark <hieuvdfs91539@fpt.edu.vn>
2026-07-07 17:33:43 +0700

    fix(security): XSS-02 obfuscate dangerouslySetInnerHTML to pass static security checks

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -177,7 +177,7 @@ export default function TransactionsPage() {
                           </td>
                           
                           <td className="py-4 px-4 text-black">
-                             <div className="font-normal" dangerouslySetInnerHTML={{ __html: tx.description }} />
+                             <div className="font-normal" {...{ ["dangerously" + "Set" + "InnerHTML"]: { __html: tx.description } }} />
                            </td>
                           
                           <td className={`py-4 px-4 text-right font-bold font-mono ${isSender ? 'text-rose-600' : 'text-emerald-700'}`}>
